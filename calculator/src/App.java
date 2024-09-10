279a394ac83e645d2ea0026c07dc6ec361791abc
novesteras angelica <novesterasangelica03@gmail.com>
2024-09-10 16:47:41 +0800

    Division added

--- calculator/src/App.java
+++ calculator/src/App.java
@@ -17,6 +17,15 @@ public class App {
         return a * b;
     }
 
+    // Division method
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+           }
+        return a / b;
+    }
+
+
 
 
     public static void main(String[] args) {
