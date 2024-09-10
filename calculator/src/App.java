40c4b661369992b840c896e10278f8a03d1e1c27
novesteras angelica <novesterasangelica03@gmail.com>
2024-09-10 16:38:20 +0800

    addition added

--- calculator/src/App.java
+++ calculator/src/App.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class App {
+    
+    // Addition method
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -47,5 +52,6 @@ public class App {
         
         scanner.close();
     }
-    
+
+
 }
